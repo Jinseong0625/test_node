67c5874f6218f08550ee68c704cd444fc70008ea
Jinseong0625 <123522700+Jinseong0625@users.noreply.github.com>
2024-01-24 10:52:01 +0900

    닉네임 추가

--- server.js
+++ server.js
@@ -16,15 +16,26 @@ wss.on('connection', (ws) => {
 
   // 클라이언트로부터 메시지를 받았을 때 실행되는 로직
   ws.on('message', (message) => {
-    console.log(`Received message: ${message}`);
+    const parsedMessage = JSON.parse(message);
+    console.log(`Received message from ${parsedMessage.nickname}: ${parsedMessage.content}`);
+  
 
     // 모든 클라이언트에게 메시지 전송
-    wss.clients.forEach((client) => {
+    /*wss.clients.forEach((client) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(message);
-      }
+      }*/
+
+      // 모든 클라이언트에게 메시지 전송
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify({
+            nickname: parsedMessage.nickname,
+            content: parsedMessage.content
+          }));
+        }
+      });
     });
-  });
 
   // 클라이언트가 연결을 종료했을 때 실행되는 로직
   ws.on('close', () => {
